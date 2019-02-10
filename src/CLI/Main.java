1e509aac5991683cd97c0b26a1e6a70f6dfb31e8
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2019-02-09 21:01:31 -0800

    Allow speed change

--- src/CLI/Main.java
+++ src/CLI/Main.java
@@ -19,8 +19,9 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private static final int WINDOW_WIDTH = 4000;
-    private static final int WINDOW_HEIGHT = 4000;
+    private static final int WINDOW_WIDTH = 700;
+    private static final int WINDOW_HEIGHT = 700;
+    private static final int NUMBER_OF_TURNS_PER_FRAME = 1000;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -29,7 +30,6 @@ public class Main extends Application {
                 new RandomColorPathDrawer(new Color(1, 0, 0, 1)),
                 new PngImageExporter(WINDOW_HEIGHT, WINDOW_WIDTH));
         visualizationManager.displayWindow(primaryStage);
-        visualizationManager.drawCircle(0, 0, 1);
 
         IAngleGenerator angleGenerator = new RandomAngleGenerator();
         ICoordinateCrawler coordinateCrawler = new ConstantDistanceCoordinateCrawler(0.005, angleGenerator);
@@ -42,56 +42,15 @@ public class Main extends Application {
             @Override
             public void handle(long now) {
                 Platform.runLater(() -> {
-                    Coordinate tempCoord = coordinateCrawler.randomMoveFrom(_coordinate);
-                    if (!circleExitChecker.isOutsideCircle(tempCoord)) {
-                        _coordinate = tempCoord;
+                    for (int i = 0; i < NUMBER_OF_TURNS_PER_FRAME; i++){
+                        Coordinate tempCoord = coordinateCrawler.randomMoveFrom(_coordinate);
+                        if (!circleExitChecker.isOutsideCircle(tempCoord)) {
+                            _coordinate = tempCoord;
+                        }
+                        visualizationManager.addPoint(_coordinate);
                     }
-                    visualizationManager.addPoint(_coordinate);
                 });
             }
         }.start();
-
-//        Task task = new Task() {
-//            @Override
-//            protected Object call() throws Exception {
-//
-//                Coordinate coordinate = new Coordinate(0, 0);
-//
-//                while (!circleExitChecker.isOutsideCircle(coordinate)) {
-//                    coordinate = coordinateCrawler.randomMoveFrom(coordinate);
-//                    visualizationManager.addPoint(coordinate);
-//                }
-//                return null;
-//            }
-//        };
-//
-//        Thread t = new Thread(task);
-//        t.start();
-
-//        double[] crawlDistances = new double[] { 0.8, 0.4, 0.2, 0.1, 0.05, 0.025, 0.0125, 0.00625, 0.003125, 0.0015625, 0.00078125, 0.00039062 };
-//
-//        for (double crawlDistance : crawlDistances){
-//
-//            IAngleGenerator angleGenerator = new RandomAngleGenerator();
-//            ICoordinateCrawler coordinateCrawler = new ConstantDistanceCoordinateCrawler(crawlDistance, angleGenerator);
-//            ICircleExitChecker circleExitChecker = new CircleExitChecker(1);
-//
-//            int[] turnHistory = new int[NUMBER_OF_SIMULATIONS];
-//
-//            for (int i = 0; i < NUMBER_OF_SIMULATIONS; i++){
-//                Coordinate coordinate = new Coordinate(0, 0);
-//
-//                int noOfTurns = 0;
-//                while (!circleExitChecker.isOutsideCircle(coordinate)) {
-//                    noOfTurns++;
-//                    coordinate = coordinateCrawler.randomMoveFrom(coordinate);
-//                }
-//
-//                turnHistory[i] = noOfTurns;
-//            }
-//
-//            double average = (double)Arrays.stream(turnHistory).sum() / turnHistory.length;
-//            System.out.println("Average turns for crawl distance: (" + crawlDistance + ") is:(" + average + ")");
-//        }
     }
 }
